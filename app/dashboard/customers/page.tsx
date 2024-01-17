2d967e110389a8bba5d596133ae1154f45f28b01
joe-boadi <joeboadi1@gmail.com>
2024-01-17 14:55:34 +0000

    feat: Display customer List

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,20 @@
 import { Metadata } from 'next';
 import Customer from '@/app/ui/customers/table'
+import { lusitana } from '@/app/ui/fonts';
+import CustomersTable from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import Table from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function Page() {
-  return <p>Customers Page</p>
+  return <div className='w-full'>
+     <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <Suspense fallback={<CustomersTable customers={[]} />}>
+      </Suspense>
+  </div>
 }
