7401021df56f15e8062538f02e741e8d9b7401d0
joe-boadi <joeboadi1@gmail.com>
2024-01-17 11:41:57 +0000

    Update file

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,11 @@
 import { Metadata } from 'next';
+import Customer from '@/app/ui/customers/table'
+import CustomersTable from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function Page() {
-  return <p>Customers Page</p>
+  return <><CustomersTable customers={[]}/></>
 }
