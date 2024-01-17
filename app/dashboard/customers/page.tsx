872a0b62971350d9648fcc555ef668ee66d85b68
joe-boadi <joeboadi1@gmail.com>
2024-01-17 11:46:30 +0000

    fix: Undo changes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,10 @@
 import { Metadata } from 'next';
 import Customer from '@/app/ui/customers/table'
-import CustomersTable from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function Page() {
-  return <><CustomersTable customers={[]}/></>
+  return <p>Customers Page</p>
 }
