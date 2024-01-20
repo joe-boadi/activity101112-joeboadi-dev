5d9b6d63f4da0843e7f2a0956f538eafce58496a
joe-boadi <joeboadi1@gmail.com>
2024-01-20 23:59:39 +0000

    Modify customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -4,11 +4,25 @@ import { lusitana } from '@/app/ui/fonts';
 import CustomersTable from '@/app/ui/customers/table';
 import { Suspense } from 'react';
 import Table from '@/app/ui/customers/table';
+import { customers } from '@/app/lib/placeholder-data';
+import CustomersTableSkeleton from '@/app/ui/skeletons';
+import { custom } from 'zod';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function Page() {
-  return <p>Customers Page</p>
+  return (
+    <main>
+      <div className='w-full'>
+        <div className='flex w-full items-center justify-between'>
+            <h1 className={ `${lusitana.className} text-2xl`}></h1>
+        </div>
+        <Suspense key={query + customers} fallback={<CustomersTableSkeleton/>}>
+          <CustomersTable customers={customers}/>
+        </Suspense>
+      </div>
+    </main>
+  )
 }
