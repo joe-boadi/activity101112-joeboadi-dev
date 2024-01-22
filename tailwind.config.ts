3902c4ae986383607dd639ea6b7d25aa9e61169b
joe-boadi <joeboadi1@gmail.com>
2024-01-22 01:00:10 +0000

    Install DaisyUI  + Update json file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), 
+            require("daisyui")],
 };
 export default config;
