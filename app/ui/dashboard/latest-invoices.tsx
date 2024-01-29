064fe45fef67f64ea774aaf2680eaa1172c6e000
joe-boadi <joeboadi1@gmail.com>
2024-01-29 15:00:49 +0000

    Remove unnecessary comment + Add Tailwind CSS styling

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -12,15 +12,14 @@ export default async function LatestInvoices() {
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: comment in this code when you get to this point in the course */}
-      <div className="bg-white px-6">
+      <div className="flex grow flex-col justify-between rounded-xl  bg-base-200 p-4">
+      <div className="bg-base-100 px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  'flex flex-row items-center justify-between border-primary-content py-4',
                   {
                     'border-t': i !== 0,  
                   },
@@ -38,7 +37,7 @@ export default async function LatestInvoices() {
                     <p className="truncate text-sm font-semibold md:text-base">
                       {invoice.name}
                     </p>
-                    <p className="hidden text-sm text-gray-500 sm:block">
+                    <p className="hidden text-sm text-base-content sm:block">
                       {invoice.email}
                     </p>
                   </div>
@@ -53,8 +52,8 @@ export default async function LatestInvoices() {
           })}
         </div>
         <div className="flex items-center pb-2 pt-6">
-          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          <ArrowPathIcon className="h-5 w-5 text-base-content" />
+          <h3 className="ml-2 text-sm text-base-content ">Updated just now</h3>
         </div>
       </div>
     </div>
