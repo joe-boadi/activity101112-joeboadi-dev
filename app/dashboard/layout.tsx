29e9bedb637a17027a27d327d4aab2725454182a
joe-boadi <joeboadi1@gmail.com>
2024-01-29 00:55:07 +0000

    Update and modify file

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import SideNav from '@/app/ui/dashboard/sidenav';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     // Default theme is set to light
-    <div data-theme="light" className="flex h-screen flex-col md:flex-row md:overflow-hidden"> 
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden"> 
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
