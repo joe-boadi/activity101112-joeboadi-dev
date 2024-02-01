3b496fdcd0ec3b335680ee323eaf3dbdcda203fd
joe-boadi <joeboadi1@gmail.com>
2024-02-01 02:38:59 +0000

    fix: Modify background color

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -7,7 +7,7 @@ import ThemeSwitch from '../theme-switch';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col bg-base-200 px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col bg-base-100 px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-primary p-4 md:h-40"
         href="/"
@@ -19,13 +19,12 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-base-200 md:block"></div>
-        <ThemeSwitch />
         <form
           action={async () => {
             'use server';
             await signOut();
           }}
-        > 
+        >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-base-200 p-3 text-sm font-medium hover:bg-red-500/90 hover:text-base-200/95 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
